2faab920af21b521482e44cf28299f604c9fabea
myname21-meet <myname21@meet.mit.edu>
2019-08-06 05:58:36 -0400

    fixed merge conflicts

--- database.py
+++ database.py
@@ -66,8 +66,3 @@ def query_all_admins():
 	return session2.query(Admin).all()
 
 # END ADMIN CODE
-<<<<<<< HEAD
-
-add_admin("hello", "hello")
-=======
->>>>>>> 98ee279424e53ee3160438f2839666f7c048c536
